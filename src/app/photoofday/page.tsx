9f18ef953b9bd3cd2502aeec8855cebf5fdb49f4
devnsko <20denysko05@gmail.com>
2024-07-24 18:04:12 +0200

    Apod basic

--- src/app/photoofday/page.tsx
+++ src/app/photoofday/page.tsx
@@ -1,18 +1,63 @@
-import Image from "next/image";
+'use client';
 
-export default function page() {
+import Image from "next/image";
+import { fetchData } from "@/tools/api";
+import { useSearchParams } from "next/navigation";
+import React, { useState, useEffect } from "react";
+import Calendar from "react-calendar";
+
+// export const revalidate = 3600;
+
+export default function Page() {
+  const params = useSearchParams();
+  const dateParam = params.get('date') ?? new Date().toISOString().split('T')[0];
+  const [value, onChange] = useState(new Date());
+  const [data, setData] = useState<{ title: string; date: string; hdurl: string; explanation: string } | null>(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchDataAsync = async () => {
+      console.log('Fetching data for date:', dateParam);
+      const fetchedData = await fetchData(`/today?date=${dateParam}`);
+      setData(fetchedData);
+      setLoading(false);
+    };
+
+    fetchDataAsync();
+  }, [dateParam]);
+
+  if (loading) {
+    return (
+      <div className="spinner-border position-absolute top-50 start-50 translate-middle" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  }
+
+  if (!data) {
+    return (
+      <div className="spinner-border" role="status">
+        <span className="visually-hidden">Loading...</span>
+      </div>
+    );
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
+      <div className="position-absolute top-50 start-50 translate-middle">
+        <p className="text-center">{data.date.split('T')[0]}</p>
+        <h1 className="text-center h3">{data.title}</h1>
+        <div className="flex flex-col items-center mx-auto p-3">
+          <Image
+            className="align-middle"
+            src={data.hdurl}
+            alt="Next.js Logo"
+            width={740}
+            height={148}
+            priority
+            />
+        </div>
+        <p className="text-center">{data.explanation}</p>
       </div>
     </main>
   );
---
+++ src/components/Header.tsx
@@ -0,0 +1,27 @@
+import Link from 'next/link';
+import React from 'react';
+
+const Header: React.FC = () => {
+  return (
+    <header>
+        <nav className="navbar" style={{ backgroundColor: '#e3f2fd' }}>
+        <div className="d-flex justify-content-between container">
+            <div className='d-flex align-items-center'>
+                <Link className="btn btn-dark mx-auto p-2" href="/" role="button">Lunapp</Link>
+                <div>
+                    <Link className="link-dark link-offset-2 link-underline-opacity-25 mx-auto p-2" href="/photoofday" role="button">Apod</Link>
+                    <Link className="link-dark link-offset-2 link-underline-opacity-25 mx-auto p-2" href="/near" role="button">Neo</Link>
+                </div>
+            </div>
+            <form className="d-flex" role="search">
+                <input className="form-control me-2" type="search" placeholder="Not working!" aria-label="Search"/>
+                <button className="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+        
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
