dcf55a4a0010b065ae14a9391def6cce15fbcc71
devnsko <20denysko05@gmail.com>
2024-07-29 21:21:27 +0000

    from remote changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from "next";
 import 'bootstrap/dist/css/bootstrap.css';
 import "./globals.css";
 import { Inter } from "next/font/google";
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
